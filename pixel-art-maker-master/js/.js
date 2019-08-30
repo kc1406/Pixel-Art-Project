6cb7e2a25b7ca4fd01eb55d9dc2136f97b83161e
kc1406 <48004101+kc1406@users.noreply.github.com>
2019-08-30 00:39:30 -0700

    clash free time table

---
+++ pixel-art-maker-master/js/.js
@@ -0,0 +1,27 @@
+//Color input
+let Color = document.getElementById('colorPicker');
+let table = docment.getElementById('pixelCanvas'); 
+let sizePicker = document.getElementById('sizePicker');
+
+
+
+sizePicker.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e + sizePicker.value);
+
+
+})
+
+
+
+
+
+//Select size input
+
+//when siz is submitted by user, call makeGrid()
+
+function makeGrid() {
+
+//Your code goes here
+
+}
